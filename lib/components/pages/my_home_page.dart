8ad37aed84cc60791ec13a42de169c256d9ffd6b
Rizwan Nasir <97438351+RizwanNaasir@users.noreply.github.com>
2023-05-30 10:43:08 +0500

    reafactored: cardList

--- lib/components/pages/my_home_page.dart
+++ lib/components/pages/my_home_page.dart
@@ -1,11 +1,13 @@
+import 'dart:convert';
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
-import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:motivation/Models/daily_quotes.dart';
 import 'package:shimmer/shimmer.dart';
+
 import '../nav_drawer.dart';
+import '../widgets/card_list.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -49,7 +51,6 @@ class _MainState extends State<MyHomePage> {
   // Main build function
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home Page'),
@@ -63,96 +64,22 @@ class _MainState extends State<MyHomePage> {
         child: Center(
             child: isLoading
                 ? _buildLoadingShimmer() // Show shimmer effect while loading
-                : ListView.builder(
-          itemCount: dailyQuotes.length,
-          itemBuilder: (context, i) {
-            return Card(
-              // Set the shape of the card using a rounded rectangle border with a 8 pixel radius
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              // Set the elevation to 3
-              elevation: 4,
-              // Set the clip behavior of the card
-              clipBehavior: Clip.antiAliasWithSaveLayer,
-              // Define the child widgets of the card
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  // Display an image at the top of the card that fills the width of the card and has a height of 160 pixels
-                  // Add a container with padding that contains the card's title, text, and buttons
-                  Container(
-                    padding: const EdgeInsets.fromLTRB(15, 15, 15, 0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        // Display the card's title using a font size of 24 and a dark grey color
-                        Text(
-                          dailyQuotes[i].content.toString(),
-                          style: TextStyle(
-                            fontSize: 12,
-                            color: Colors.grey[800],
-                          ),
-                        ),
-                        // Add a space between the title and the text
-                        Container(height: 10),
-                        // Display the card's text using a font size of 15 and a light grey color
-                        Text(
-                          dailyQuotes[i].author.toString(),
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: Colors.grey[700],
-                          ),
-                        ),
-                        // Add a row with two buttons spaced apart and aligned to the right side of the card
-                        Row(
-                          children: <Widget>[
-                            // Add a spacer to push the buttons to the right side of the card
-                            const Spacer(),
-                            IconButton(
-                              style: TextButton.styleFrom(
-                                foregroundColor: Colors.transparent,
-                              ),
-                              icon: Icon(
-                                Icons.favorite,
-                                color: (dailyQuotes[i].isLiked!)
-                                    ? Colors.red
-                                    : grey,
-                                size: 24.0,
-                                semanticLabel:
-                                    'Text to announce in accessibility modes',
-                              ),
-                              onPressed: () {
-                                setState(() {
-                                  dailyQuotes[i].isLiked =
-                                      !dailyQuotes[i].isLiked!;
-                                });
-                              },
-                            ),
-                            // Add a text button labeled "EXPLORE" with transparent foreground color and an accent color for the text
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  // Add a small space between the card and the next widget
-                  Container(height: 5),
-                ],
-              ),
-            );
-          },
-        )),
+                : cardList(
+                    context, dailyQuotes, setState) // Show the list of quotes
+            ), // Show the list of quotes
       ),
     );
   }
 }
+
 Widget _buildLoadingShimmer() {
   return ListView.builder(
     itemCount: 5, // Number of shimmer placeholders to show
     itemBuilder: (context, i) {
       return Shimmer.fromColors(
         baseColor: const Color(0xFF808080), // Color of the shimmer base color
-        highlightColor: const Color(0xFFB0B0B0), // Color of the shimmer highlight
+        highlightColor:
+            const Color(0xFFB0B0B0), // Color of the shimmer highlight
         child: Container(
           margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
           decoration: BoxDecoration(
